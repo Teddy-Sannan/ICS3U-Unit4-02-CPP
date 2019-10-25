60ba38c937888a3e83b27fe46f89fee062b29821
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-10-25 16:48:57 +0000

    factorial number

---
+++ factorial_number.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Teddy Sannan
+// Created on: October 2019
+// This program gets user to input their age with user input
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main() {
+    // inputs
+    std::string integerAsString;
+    int integerAsNumber = 0;
+    int loopCounter = 0;
+    int total = 1;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> integerAsString;
+    std::cout << "" << std::endl;
+
+    try {
+        integerAsNumber = std::stoi(integerAsString);
+        do {
+            std::cout <<" x" << loopCounter << std::endl;
+            loopCounter = loopCounter + 1;
+            total = total * loopCounter;
+        } while (loopCounter < integerAsNumber);
+        std::cout << "Your total is " << total << std::endl;
+    } catch (std::invalid_argument) {
+            std::cout << "Not a valid integer" << std::endl;
+    }
+}
